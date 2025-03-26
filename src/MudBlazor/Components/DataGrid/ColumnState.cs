54b558ffcd1c3e49966c8fe6fb7f6e05853b2659
pascal <pascal.meheut@gmail.com>
2025-03-26 16:32:14 +0100

    working on table columns state save

---
+++ src/MudBlazor/Components/DataGrid/ColumnState.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace MudBlazor;
+
+public record ColumnState(string Name, bool Hidden, SortDirection SortDirection, double? Width);
+
+public record ColumnsStateEvent();
